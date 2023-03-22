95def934732973aa8f2ac025218f2cf3cba339c3
Etta <etta.lioness@hotmail.com>
2023-03-22 12:43:36 +1100

    Created Event Models/view/urls/seriaizers
    Created EventList and EventDetail: Models/view/urls/serializers
    All tested and passed on insomnia
    GET, PUT, POST, DELETE

--- schedulingtool/mentor/models.py
+++ schedulingtool/mentor/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model #Django will find what model you are using
+from datetime import datetime, timedelta
 
+User = get_user_model()
+
+event_category = (
+    ("Flash", "Flash"),
+    ("Plus", "Plus"),
+    ("One Day Workshop", "One Day Workshop")
+)
+
+location_category = (
+    ("Sydney", "Sydney"),
+    ("Perth", "Perth"),
+    ("Brisbane", "Brisbane")
+)
 # Create your models here.
+class Event(models.Model):
+    image=models.URLField(default="https://shecodes.com.au/wp-content/uploads/2020/02/Purple_no_circle.svg")
+    event_type=models.CharField(max_length=200, null=True, choices= event_category)
+    location=models.CharField(max_length=200, null=True, choices= location_category)
+    description=models.TextField()
+    start_date = models.DateTimeField(default=datetime.now)
+    end_date = models.DateTimeField(default=(datetime.now() + timedelta(days=30)))
+    status= models.BooleanField(default=True)
+    
+
--- schedulingtool/mentor/views.py
+++ schedulingtool/mentor/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Event
+from .serializers import EventSerializer,EventDetailSerializer
+from django.http import Http404
+from rest_framework import status, generics, permissions
+# from .permissions import IsOwnerOrReadOnly, IsSupporterOrReadOnly
 
 # Create your views here.
+class EventList(APIView):
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly] #built in django to allow only logged in
+    
+    def get(self, request):
+        event = Event.objects.all().values()
+        if request.data.get("location_category"):
+            event = Event.filter(location_category=request.data.get("location_category"))
+        if request.data.get("event_category"):
+            event = Event.filter(event_category=request.data.get("event_category"))
+        serializer = EventSerializer(event, many=True) #get list of many projects not one
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = EventSerializer(data=request.data)# use the data that was given from user
+        if serializer.is_valid():#built in serializer checks info given is ok, as expected
+            serializer.save()
+            # serializer.save(owner=request.user)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+                )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+class EventDetail(APIView):
+    # permission_classes = [
+    #     permissions.IsAuthenticatedOrReadOnly,
+    #     IsOwnerOrReadOnly
+    # ]
+
+    def get_object(self, pk):
+        try:
+            # return Project.objects.get(pk=pk) #passing the input in as an attribute
+            event = Event.objects.get(pk=pk)
+            self.check_object_permissions(self.request, event)
+            return event
+        except Event.DoesNotExist:
+            raise Http404 #means resource does not exit, user did something wrong
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = EventDetailSerializer(project)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        event = self.get_object(pk)
+        data = request.data
+        serializer = EventDetailSerializer(
+            instance = event,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+            ####Added what happens if not valid like above
+
+    def delete(self, request, pk, format=None):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
