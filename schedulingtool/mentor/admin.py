f6d8331e0a8975e5b92cf27f257f648e8eed1799
Etta <etta.lioness@hotmail.com>
2023-04-03 16:58:06 +1000

    added admin access for tech_stack and onboarding, also added to .dockerignore .git and *.sqlite3

--- schedulingtool/mentor/admin.py
+++ schedulingtool/mentor/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Event, Mentor
+from .models import Event, Mentor, Onboarding, Tech_Stack
 
 admin.site.register(Event)
 admin.site.register(Mentor)
+admin.site.register(Onboarding)
+admin.site.register(Tech_Stack)
